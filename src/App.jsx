caddf0b97a6161d3909d2b4592ef2a214d5baef2
Natalí Trindade <salles.natali@gmail.com>
2024-03-30 14:19:09 -0300

    feat(src/app.jsx): add global styles reset component
    in addition to adding the project reset, the commit was also used to insert the navigation tab logo
    into the index.html file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    background: #141414;
+    font-family: "Inter", sans-serif;
+    font-display: swap;
+    font-size: 1rem;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
 };
 
 export default App;
